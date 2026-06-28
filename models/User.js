a0adfe626673d16e6b39b206d947a214720e46dd
Marsudi505 <rchyt913@gmail.com>
2026-06-28 23:31:19 +0700

    Upload models/User.js

---
+++ models/User.js
@@ -0,0 +1,11 @@
+// models/User.js
+
+const mongoose = require("mongoose")
+
+module.exports = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    username: String,
+    password: String
+  })
+)
